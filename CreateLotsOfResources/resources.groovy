f93538314e07ebad0c7abf5353178327ec357853
U-ELECTRIC-CLOUD\mmorales <mmorales@e7480-mmorales.electric-cloud.com>
2017-08-29 17:09:36 -0400

    Add a new project to create over 100 resources into different pools.  The Resources are based on the elements in the periodic table. Provided as an example.

---
+++ CreateLotsOfResources/resources.groovy
@@ -0,0 +1,46 @@
+// Iterate over all the items in our list  create resources, resourcePools, and the Environments
+
+args.elements.each { element ->
+	// For now, the name of the element is the location 
+	def elementName = element.Element
+
+	// The resourcePool is defined by a comma-separated list, which we create here for later use.
+	def poolList = element.Subcategory + ", " + ", group-" + element.Group + ", period-" + element.Period
+	
+	resource elementName, {
+		description = element.Description
+		artifactCacheDirectory = null
+		// TODO: This hostName should become a lookup
+		hostName = element.Hostname
+		port = '7800'
+		proxyCustomization = null
+		proxyHostName = null
+		proxyProtocol = null
+		repositoryNames = null
+		resourceDisabled = '0'
+		shell = null
+		trusted = '0'
+		useSSL = '1'
+		workspaceName = null
+		zoneName = 'default'
+		resourcePools = poolList
+    }
+
+	//Define environments for Store resources.  There is a 1:1 mapping for resource to environments
+	project args.projName, {
+		environment elementName, {
+		    environmentEnabled = '1'
+			projectName = args.projName
+			reservationRequired = '0'
+			rollingDeployEnabled = null
+			rollingDeployType = null
+
+			environmentTier args.envTier, {
+				batchSize = null
+				batchSizeType = null
+				resourceName = elementName
+			}
+		}
+	}
+}
+
