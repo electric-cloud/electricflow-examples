fcf089164b87e7ea93f1fa7eb96fa78d8380f038
Marco Morales <mmorales@electric-cloud.com>
2017-12-12 10:15:32 -0500

    Fixed-up environment tiers.  Those now work.

--- ComplexPipeline/environments.groovy
+++ ComplexPipeline/environments.groovy
@@ -2,25 +2,21 @@ def myProject = args.project
 
 //Define environments for this project
 project myProject.name, {
-    args.resources.each { myResource ->
-        environment myResource, {
+    myProject.environments?.each { myEnvironment->
+        environment myEnvironment.name, {
+            description = myEnvironment.description
             environmentEnabled = '1'
             projectName = myProject.name
             reservationRequired = '0'
             rollingDeployEnabled = null
             rollingDeployType = null
 
-            // TODO: These environment Tiers should be looked up from JSON, not hard-coded here.
-
-            environmentTier 'AppTier', {
-                batchSize = null
-                batchSizeType = null
-                resourceName = myResource
-            }
-            environmentTier 'DBTier', {
-                batchSize = null
-                batchSizeType = null
-                resourceName = myResource
+            myEnvironment.environmentTiers?.each {myEnvironmentTier ->
+                environmentTier myEnvironmentTier.name, {
+                    batchSize = null
+                    batchSizeType = null
+                    resourceName = myEnvironmentTier.resources
+                }
             }
         }
     }
