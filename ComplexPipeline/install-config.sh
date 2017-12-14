36e2a3d42158367128b19dab783df26eed5cf86e
Marco Morales <mmorales@electric-cloud.com>
2017-12-14 09:48:39 -0500

    remove files no longer needed.

--- ComplexPipeline/install-config.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Add Configurations, Credentials, ACL entries, special users for email notifications
-
-echo "This file has been deprecated.  See the plain-old 'install.sh' to run configuration."
