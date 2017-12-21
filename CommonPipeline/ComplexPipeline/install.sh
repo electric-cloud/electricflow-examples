b3b235d8b08af93dccadfbfe5f451f2fcc3eb39c
Marco Morales <mmorales@electric-cloud.com>
2017-12-21 13:52:06 -0500

    Revise ComplexPipeline model with a needed field. Add a short installation script to run the parent setup. Rename the default projectname.

---
+++ CommonPipeline/ComplexPipeline/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd ..
+./install.sh -l -P ComplexPipeline/input-model.json -N ComplexPipeline -G com.ec.complex.pipeline
--- CommonPipeline/install.sh
+++ CommonPipeline/install.sh
@@ -5,7 +5,7 @@ ROOT=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 PARAMETERSFILE=input-model.json
 MYJSONFILE=model.json
-PROJECTNAME=ComplexPipeline
+PROJECTNAME=CommonPipeline
 GROUPID=com.ec.samples
 BASEDIR=.
 
