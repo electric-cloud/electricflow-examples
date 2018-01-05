6b7e89fe7482b6c6c2a8d2be27f33e77c49e9ec0
Marco Morales <mmorales@electric-cloud.com>
2018-01-05 11:07:01 -0500

    Revise resources to include both the name and hostName.  This change also lets us prepare for the future where we need to specify more details about resources.

--- CommonPipeline/resources.groovy
+++ CommonPipeline/resources.groovy
@@ -2,10 +2,10 @@ def myProject = args.project
 
 // Iterate over all the stores in our list and create resources, resourcePools, and the Environments
 args.resources.each { myResource ->
-    resource "$myResource", {
+    resource myResource.name, {
         description = "Resource for $myProject.name"
         artifactCacheDirectory = null
-        hostName = 'localhost'
+        hostName = myResource.hostName
         port = '7800'
         resourceDisabled = '0'
         trusted = '0'
