2d08fe26da270579a2f1e9c5bba668f8532d4459
Marco Morales <mmorales@electric-cloud.com>
2018-01-25 13:19:24 -0500

    Fix reference to services name Start the process by which the pipeline uses services.

--- CommonPipeline/ServicesPipeline/install.sh
+++ CommonPipeline/ServicesPipeline/install.sh
@@ -2,18 +2,19 @@
 
 GROUPID=com.ec.services
 PROJECTNAME=ServiceRelease
+SERVICESPROJECTNAME=WW
 MYJSONFILE=model.json
 MYRELJSONFILE=release-model.json
 
 # Replace tokens with real names
-sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" input-model.json > $MYJSONFILE.project
-sed -e "s/@@GROUPID@@/$GROUPID/" $MYJSONFILE.project > $MYJSONFILE.groupid
-cp $MYJSONFILE.groupid $MYJSONFILE
+sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" \
+    -e "s/@@SERVICESPROJECTNAMETOKEN@@/$SERVICESPROJECTNAME/" \
+    -e "s/@@GROUPID@@/$GROUPID/" input-model.json > $MYJSONFILE
 
 # Replace tokens with real names
-sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" input-release.json > $MYRELJSONFILE.project
-sed -e "s/@@GROUPID@@/$GROUPID/" $MYRELJSONFILE.project > $MYRELJSONFILE.groupid
-cp $MYRELJSONFILE.groupid $MYRELJSONFILE
+sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" \
+    -e "s/@@SERVICESPROJECTNAMETOKEN@@/$SERVICESPROJECTNAME/" \
+    -e "s/@@GROUPID@@/$GROUPID/" input-release.json > $MYRELJSONFILE
 
 ./install-config.sh -c -f $MYJSONFILE -P $PROJECTNAME -G $GROUPID
 ../install.sh -A -f MultiAppRelease/$MYJSONFILE -P $PROJECTNAME -G $GROUPID
