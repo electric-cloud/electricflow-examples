7b14dcc20a0975387094847a06a0eab89fb6243f
Marco Morales <mmorales@electric-cloud.com>
2018-02-07 21:15:10 -0500

    MultiAppRelease now uses common efmodel framework.

--- efmodel/MultiAppRelease/install.sh
+++ efmodel/MultiAppRelease/install.sh
@@ -6,15 +6,15 @@ MYJSONFILE=model.json
 MYRELJSONFILE=release-model.json
 
 # Replace tokens with real names
-sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" input-model.json > $MYJSONFILE.project
-sed -e "s/@@GROUPID@@/$GROUPID/" $MYJSONFILE.project > $MYJSONFILE.groupid
-cp $MYJSONFILE.groupid $MYJSONFILE
+sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" \
+    -e "s/@@SERVICEPROJECTNAMETOKEN@@/$SERVICESPROJECTNAME/" \
+    -e "s/@@GROUPID@@/$GROUPID/" input-model.json > $MYJSONFILE
 
 # Replace tokens with real names
-sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" input-release.json > $MYRELJSONFILE.project
-sed -e "s/@@GROUPID@@/$GROUPID/" $MYRELJSONFILE.project > $MYRELJSONFILE.groupid
-cp $MYRELJSONFILE.groupid $MYRELJSONFILE
+sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" \
+    -e "s/@@SERVICEPROJECTNAMETOKEN@@/$SERVICESPROJECTNAME/" \
+    -e "s/@@GROUPID@@/$GROUPID/" input-release.json > $MYRELJSONFILE
 
 ./install-config.sh -c -f $MYJSONFILE -P $PROJECTNAME -G $GROUPID
-../install.sh -A -f MultiAppRelease/$MYJSONFILE -P $PROJECTNAME -G $GROUPID
-../install.sh -R -f $MYRELJSONFILE -P $PROJECTNAME -G $GROUPID
+efmodel.sh -A -f $MYJSONFILE -P $PROJECTNAME -G $GROUPID
+efmodel.sh -R -f $MYRELJSONFILE -P $PROJECTNAME -G $GROUPID
