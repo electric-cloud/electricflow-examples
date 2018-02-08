aa125c836b6a015060dc462dbf70e5211a151d0e
Marco Morales <mmorales@electric-cloud.com>
2018-02-07 21:31:04 -0500

    Fix up pre-run parsing and substitution of tokens. Fix broken environment reference.

--- efmodel/WeaveWorks/install.sh
+++ efmodel/WeaveWorks/install.sh
@@ -21,15 +21,23 @@ while getopts ":G:N:P:" opt; do
   esac
 done
 
+for json in "weaveworks" "weaveworks-carts-service" "weaveworks-catalog-service" "weaveworks-orders-service" "weaveworks-services" "weaveworks-user-service" "weaveworks-zipkin-service"; do
+    echo "transforming $json"
 
+    # Replace tokens with real names
+    sed -e "s/@@PROJECTNAMETOKEN@@/$PROJECTNAME/" \
+        -e "s/@@SERVICEPROJECTNAMETOKEN@@/$SERVICESPROJECTNAME/" \
+        -e "s/@@GROUPID@@/$GROUPID/" $json.json > new-$json.json
+
+done
 # Parse command line
 # Only a few options in place - see the help for details and update as needed.
 #./install.sh -c -t -r -N WW -G com.ww.demo -P weaveworks.json
-../install.sh -e -P $PROJECTNAME -G $GROUPID -f weaveworks.json
-../install.sh -r -P $PROJECTNAME -G $GROUPID -f weaveworks.json
-../install.sh -s -P $PROJECTNAME -G $GROUPID -f weaveworks-carts-service.json
-../install.sh -s -P $PROJECTNAME -G $GROUPID -f weaveworks-catalog-service.json
-../install.sh -s -P $PROJECTNAME -G $GROUPID -f weaveworks-orders-service.json
-../install.sh -s -P $PROJECTNAME -G $GROUPID -f weaveworks-services.json
-../install.sh -s -P $PROJECTNAME -G $GROUPID -f weaveworks-user-service.json
-../install.sh -s -P $PROJECTNAME -G $GROUPID -f weaveworks-zipkin-service.json
+efmodel.sh -e -P $PROJECTNAME -G $GROUPID -f new-weaveworks.json
+efmodel.sh -r -P $PROJECTNAME -G $GROUPID -f new-weaveworks.json
+efmodel.sh -s -P $PROJECTNAME -G $GROUPID -f new-weaveworks-carts-service.json
+efmodel.sh -s -P $PROJECTNAME -G $GROUPID -f new-weaveworks-catalog-service.json
+efmodel.sh -s -P $PROJECTNAME -G $GROUPID -f new-weaveworks-orders-service.json
+efmodel.sh -s -P $PROJECTNAME -G $GROUPID -f new-weaveworks-services.json
+efmodel.sh -s -P $PROJECTNAME -G $GROUPID -f new-weaveworks-user-service.json
+efmodel.sh -s -P $PROJECTNAME -G $GROUPID -f new-weaveworks-zipkin-service.json
