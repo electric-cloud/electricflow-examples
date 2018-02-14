22372389fd22b4032b3e84ae81439686a31b1d94
Marco Morales <mmorales@electric-cloud.com>
2018-02-14 10:28:15 -0500

    Wrap the schedule definition with a project declaration. Also, add some null assignments.

--- efmodel/configuration.groovy
+++ efmodel/configuration.groovy
@@ -33,32 +33,64 @@ args.users.each { myUser ->
 }
 
 args.schedules.each { mySchedule ->
-    schedule mySchedule.name, {
-        description = mySchedule.description
-        misfirePolicy = mySchedule.misfirePolicy
-        priority = mySchedule.priority
-        // TODO: The logic behind the CI Manager is broken.  We can't use a reference such as "/projects[projectname]/procedures[myproc]"
-        // Instead, we have to use /projects/projectname/procedures/myproc
-        procedureName = mySchedule.procedureName
-        projectName = mySchedule.projectName
-        releaseName = mySchedule.releaseName
-        scheduleDisabled = mySchedule.scheduleDisabled
-        timezone = mySchedule.timezone
-        actualParameter = mySchedule.actualParameters?.collectEntries {aParam->
-            [
-                    (aParam.name) : aParam.value,
-            ]
-        }
-        property 'ec_customEditorData', {
-            GitBranch = mySchedule.GitBranch
-            GitRepo = mySchedule.GitRepo
-            QuietTimeMinutes = mySchedule.QuietTimeMinutes
-            TriggerFlag = mySchedule.TriggerFlag
-            dest = mySchedule.dest
-//            formType = '$[/plugins/ECSCM-Git/project/scm_form/sentry]'
-            lsRemote = '1'
-            monitorTags = '0'
-            scmConfig = mySchedule.scmConfig
+    project myProject.name, {
+        schedule mySchedule.name, {
+            description = mySchedule.description
+            interval = null
+            intervalUnits = null
+            misfirePolicy = mySchedule.misfirePolicy
+            priority = mySchedule.priority
+            // TODO: The logic behind the CI Manager is broken.  We can't use a reference such as "/projects[projectname]/procedures[myproc]"
+            // Instead, we have to use /projects/projectname/procedures/myproc
+            procedureName = mySchedule.procedureName
+//           projectName = mySchedule.projectName
+//            releaseName = mySchedule.releaseName
+            scheduleDisabled = mySchedule.scheduleDisabled
+            timezone = mySchedule.timezone
+
+            // One or more of these appear to be needed to allow us to specify the procedure name.
+            applicationName = null
+            applicationProjectName = null
+            beginDate = null
+            endDate = null
+            environmentName = null
+            environmentProjectName = null
+            environmentTemplateName = null
+            environmentTemplateProjectName = null
+            environmentTemplateTierMapName = null
+            monthDays = null
+            pipelineName = null
+            processName = null
+            releaseName = null
+            rollingDeployEnabled = null
+            rollingDeployManualStepAssignees = null
+            rollingDeployManualStepCondition = null
+            rollingDeployPhases = null
+            serviceName = null
+            snapshotName = null
+            startTime = null
+            startingStage = null
+            startingStateName = null
+            stopTime = null
+            weekDays = null
+            workflowName = null
+
+//            actualParameter = mySchedule.actualParameters?.collectEntries { aParam ->
+//                [
+//                        (aParam.name): aParam.value,
+//                ]
+//            }
+            property 'ec_customEditorData', {
+                GitBranch = mySchedule.GitBranch
+                GitRepo = mySchedule.GitRepo
+                QuietTimeMinutes = mySchedule.QuietTimeMinutes
+                TriggerFlag = mySchedule.TriggerFlag
+                dest = mySchedule.dest
+                //            formType = '$[/plugins/ECSCM-Git/project/scm_form/sentry]'
+                lsRemote = '1'
+                monitorTags = '0'
+                scmConfig = mySchedule.scmConfig
+            }
         }
     }
 }
